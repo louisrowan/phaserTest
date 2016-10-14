d04892f0b486cf30996fda1c41fb4b08c7dac52f
Louis Rowan <email": null>
2016-10-14 14:05:06 -0700

    adds feature to eat bugs

--- app/assets/javascripts/phasergame.js
+++ app/assets/javascripts/phasergame.js
@@ -1,32 +1,98 @@
 var game = new Phaser.Game(1000, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 var cursors;
+var score = 0;
 
 function preload() {
   game.load.image('tux', '/assets/frog.png');
   game.load.image("background", "/assets/street.jpeg");
   game.load.image('car1', '/assets/car1.png');
+  game.load.image('car2', '/assets/car2.png')
+  game.load.image('bug', '/assets/bug.png')
 }
 
 function create() {
   game.add.tileSprite(0, 0, 1000, 600, 'background');
   game.physics.startSystem(Phaser.Physics.ARCADE);
-  cars = game.
+
+  cars = game.add.group();
+  cars.enableBody = true;
+  createCar(game.width/2, 0);
+  createCar(game.width, 400)
+
+  rightCars = game.add.group();
+  rightCars.enableBody = true;
+  createRightCar(0, 200)
 
   players = game.add.group();
   players.enableBody = true;
   createPlayer(0, 10, -500, 500);
 
-  platforms = game.add.group();
-  platforms.enableBody = true;
-  //createPlatform();
+  bugs = game.add.group();
+  bugs.enableBody = true;
+  createBug(300, 300);
+
+
   cursors = game.input.keyboard.createCursorKeys();
   game.input.onDown.add(go_fullscreen, this);
+
+  scoreText = game.add.text(32, 550, 'bugs eaten: ', { font: '20px Arial', fill: '#ffffff', align: 'left'});
 }
 
 function update() {
   playerUpdate();
+  carUpdate();
+  rightCarUpdate();
+
+  game.physics.arcade.overlap(players, bugs, playerBugCollisionHandler, null, this);
+  game.physics.arcade.overlap(cars, bugs, carBugCollisionHandler, null, this);
+  game.physics.arcade.overlap(rightCars, bugs, carBugCollisionHandler, null, this);
 }
+
+function carBugCollisionHandler(car, bug){
+  bug.kill();
+  createBug(Math.random()*(game.width - 0), Math.random()*(game.height - 0))
+}
+
+function playerBugCollisionHandler(player, bug){
+  bug.kill();
+  score += 1;
+  createBug(Math.random()*(game.width - 0), Math.random()*(game.height - 0))
+  scoreText.text = 'bugs eaten: ' + score;
+}
+
+function createBug(x, y){
+  var bug = bugs.create(x, y, 'bug')
+}
+
+function createRightCar(x, y){
+  var rightCar = rightCars.create(x, y, 'car2')
+  rightCar.body.immovable = true;
+}
+
+function createCar(x,y){
+  var car = cars.create(x, y, 'car1')
+  car.body.immovable = true;
+}
+
+function rightCarUpdate(){
+  rightCars.forEach(function(c){
+    c.body.velocity.x = 400;
+    if (c.x > game.width + 300){
+      c.x = -300;
+    }
+  })
+}
+
+function carUpdate(){
+  cars.forEach(function(c){
+    c.body.velocity.x = -400;
+    if (c.x < -300 ){
+      c.x = game.width + 400;
+    }
+  })
+}
+
 
 function createPlayer(x, y, j, v){
   var player = players.create(x, y, 'tux');
@@ -38,8 +104,9 @@ function createPlayer(x, y, j, v){
 }
 
 function playerUpdate(){
-  game.physics.arcade.collide(players, players)
-  game.physics.arcade.collide(players, platforms);
+  game.physics.arcade.collide(players, players);
+  game.physics.arcade.collide(players, cars);
+  game.physics.arcade.collide(players, rightCars);
   players.forEach(function(p){
     p.body.velocity.x = 0;
     if(cursors.left.isDown){
@@ -57,12 +124,7 @@ function playerUpdate(){
   })
 }
 
-function createPlatform(){
-  for(var i = 0; i < game.world.width; i+= 64){
-    var ground = platforms.create(i, game.world.height - 64, 'brick');
-    ground.body.immovable = true;
-  }
-}
+
 
 function go_fullscreen(){
   game.scale.fullScreenScaleMode = Phaser.ScaleManager.SHOW_ALL;
